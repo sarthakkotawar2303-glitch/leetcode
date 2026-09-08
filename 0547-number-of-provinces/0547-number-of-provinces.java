4afbb6c1aba332b7de9f5700af33e0db055fc202
sarthakkotawar2303 <sarthakkotawar2303@gmail.com>
2026-09-08 16:56:52 +0530

    Time: 1 ms (86.06%), Space: 47.1 MB (87.98%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,28 +1,59 @@
+class dsj {
+    int[] parent;
+    int[] size;
+    int NoOfComponents = 0;
+
+    dsj(int n){
+        parent=new int[n];
+        size=new int[n];
+
+        for(int i=1;i<n;i++){
+           parent[i]=i;
+           size[i]=1;
+        }
+    }
+
+    public int find(int a) {
+        if (parent[a] == a)
+            return a;
+
+        return parent[a] = find(parent[a]);
+    }
+
+    public void union(int a,int b){
+        int parentA=find(a);
+        int parentB=find(b);
+        
+
+        if(parentA==parentB) return;
+        NoOfComponents++;
+
+        if(size[parentA]<=size[parentB]){
+            parent[parentA]=parentB;
+            size[parentB]+=size[parentA];
+        }else{
+            parent[parentB]=parentA;
+            size[parentA]+=size[parentB];
+        }
+
+        return;
+    }
+}
+
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n=isConnected.length;
-       boolean[] visited =new boolean[n+1];
-       Queue<Integer>q=new LinkedList<>();
-       int provinces=0;
+        dsj d=new dsj(n+1);
 
-       for(int i=0;i<n;i++){
-        if(!visited[i]){
-            visited[i]=true;
-            q.offer(i);
-            provinces++;
-
-            while(!q.isEmpty()){
-                 int node=q.poll();
-                  for(int j=0;j<isConnected[0].length;j++){
-                 if(isConnected[node][j]==1 && !visited[j]){
-                    visited[j]=true;
-                    q.offer(j);
-                 }
-            }
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(isConnected[i][j]==1){
+                    d.union(i,j); 
+                }
             }
         }
 
-       }
-       return provinces;
+        return n-d.NoOfComponents;
+
     }
 }
