c0036e3774f08660317639dc29a8ef9cae560ec9
sarthakkotawar2303 <sarthakkotawar2303@gmail.com>
2026-09-18 16:45:11 +0530

    Time: 0 ms (100%), Space: 42.4 MB (17.47%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,21 +1,20 @@
 class Solution {
-    public int climbStairs(int n) {
-        int[] memo = new int[n + 1];
-        return totalWays(n, memo);
-    }
-    
-    private int totalWays(int n, int[] memo) {
-        // Base cases
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-        
-        // Check if value was already calculated
-        if (memo[n] != 0) {
-            return memo[n];
+    int[] dp;
+
+    public int compute(int n){
+
+        if(n==2) return 2;
+        if(n==1) return 1;
+
+        if (dp[n] != 0) {
+            return dp[n];
         }
-        
-        // Save result to cache before returning
-        memo[n] = totalWays(n - 1, memo) + totalWays(n - 2, memo);
-        return memo[n];
+
+        return dp[n]=compute(n-1)+compute(n-2);
+
+    }
+    public int climbStairs(int n) {
+        dp=new int[n+1];
+        return compute(n);
     }
 }
