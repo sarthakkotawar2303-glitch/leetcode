95e0d4256fd46b1c888a66a42c1d68cbdac7ca90
sarthakkotawar2303 <sarthakkotawar2303@gmail.com>
2026-09-21 22:36:59 +0530

    Time: 0 ms (100%), Space: 42.6 MB (74.94%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,45 @@
+
+//    BASIC RECURSIVE APPROACH
+
+// class Solution {
+//     public int maxAmt(int idx,int[] nums){
+
+//         if(idx<0) return 0;
+
+//         if(idx==0) return nums[0];
+
+//         int pick=maxAmt(idx-2,nums)+nums[idx];
+//         int notPick=maxAmt(idx-1,nums);
+
+//         return Math.max(pick,notPick);
+//     }
+//     public int rob(int[] nums) {
+//         return maxAmt(nums.length-1,nums);
+//     }
+// }
+
+
+// MEMORIZATION
+
+class Solution {
+
+    int[] dp;
+    public int maxAmt(int idx,int[] nums){
+
+        if(idx<0) return 0;
+
+        if(idx==0) return nums[0];
+
+        if(dp[idx]!=-1) return dp[idx];
+
+        int pick=maxAmt(idx-2,nums)+nums[idx];
+        int notPick=maxAmt(idx-1,nums);
+
+        return dp[idx]=Math.max(pick,notPick);
+    }
+    public int rob(int[] nums) {
+        dp=new int[nums.length];
+        Arrays.fill(dp,-1);
+        return maxAmt(nums.length-1,nums);
+    }
+}
