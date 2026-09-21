7f9a4dedb5e08f0de23a2780d8577f953aa66835
sarthakkotawar2303 <sarthakkotawar2303@gmail.com>
2026-09-21 23:24:20 +0530

    Time: 0 ms (100%), Space: 42.5 MB (94.17%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -21,25 +21,56 @@
 
 // MEMORIZATION
 
+// class Solution {
+
+//     int[] dp;
+//     public int maxAmt(int idx,int[] nums){
+
+//         if(idx<0) return 0;
+
+//         if(idx==0) return nums[0];
+
+//         if(dp[idx]!=-1) return dp[idx];
+
+//         int pick=maxAmt(idx-2,nums)+nums[idx];
+//         int notPick=maxAmt(idx-1,nums);
+
+//         return dp[idx]=Math.max(pick,notPick);
+//     }
+//     public int rob(int[] nums) {
+//         dp=new int[nums.length];
+//         Arrays.fill(dp,-1);
+//         return maxAmt(nums.length-1,nums);
+//     }
+// }
+
+
+//TABULATION 
+
+
 class Solution {
 
-    int[] dp;
-    public int maxAmt(int idx,int[] nums){
 
-        if(idx<0) return 0;
-
-        if(idx==0) return nums[0];
-
-        if(dp[idx]!=-1) return dp[idx];
-
-        int pick=maxAmt(idx-2,nums)+nums[idx];
-        int notPick=maxAmt(idx-1,nums);
-
-        return dp[idx]=Math.max(pick,notPick);
-    }
     public int rob(int[] nums) {
-        dp=new int[nums.length];
-        Arrays.fill(dp,-1);
-        return maxAmt(nums.length-1,nums);
+        int p1=nums[0];
+        int p2=0;
+
+        for(int i=1;i<nums.length;i++){
+            int pick=nums[i];
+
+            if(i>1) pick+=p2;
+
+            int notPick=0+p1;
+
+            int curr=Math.max(pick,notPick);
+
+            p2=p1;
+            p1=curr;
+        }
+
+        return p1;
     }
 }
+
+
+
