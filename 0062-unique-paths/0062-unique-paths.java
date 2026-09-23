9c57f365d6f7de227d2ad6f2a04bb3dffbd1f5dd
sarthakkotawar2303 <sarthakkotawar2303@gmail.com>
2026-09-23 11:58:47 +0530

    Time: 1 ms (13.26%), Space: 42.1 MB (65.14%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,25 +1,78 @@
+// RECURSIVE APPRAOCH :- BUT TIME LIMITS OCCURS
+
+// class Solution {
+//     public int paths(int m, int n) {
+
+//         if(m==0 && n==0) return 1;
+
+//         if(m<0 || n<0) return 0;
+
+//         int down=paths(m-1,n);
+//         int right=paths(m,n-1);
+
+//         return down+right;
+//     }
+//     public int uniquePaths(int m, int n) {
+//         return paths(m-1,n-1);
+//     }
+// }
+
+// MEMORIZATION
+// class Solution {
+//     int[][]dp;
+//     public int paths(int m, int n) {
+
+//         if(m==0 && n==0) return 1;
+
+//         if(m<0 || n<0) return 0;
+
+//         if(dp[m][n]!=-1) return dp[m][n];
+
+//         int down=paths(m-1,n);
+//         int right=paths(m,n-1);
+
+//         return dp[m][n]=down+right;
+//     }
+//     public int uniquePaths(int m, int n) {
+//         dp=new int[m][n];
+
+//         for(int[] arr:dp){
+//              Arrays.fill(arr,-1);
+//         }
+//         return paths(m-1,n-1);
+//     }
+// }
+
+// TABULATION
+
 class Solution {
-    int[][]dp;
-    public int paths(int m, int n) {
-        
-        if(m==0 && n==0) return 1;
-
-        if(m<0 || n<0) return 0;
-
-        if(dp[m][n]!=-1) return dp[m][n];
-
-        
-        int down=paths(m-1,n);
-        int right=paths(m,n-1);
-
-        return dp[m][n]=down+right;
-    }
     public int uniquePaths(int m, int n) {
-        dp=new int[m][n];
+        int[][]dp = new int[m][n];
 
-        for(int[] arr:dp){
-             Arrays.fill(arr,-1);
+        for (int[] arr : dp) {
+            Arrays.fill(arr, -1);
         }
-        return paths(m-1,n-1);
+
+        dp[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (i == 0 && j == 0)
+                    continue;
+
+                int right = 0;
+                int down = 0;
+
+                if (i > 0) {
+                    down = dp[i - 1][j];
+                }
+                if (j > 0) {
+                    right = dp[i][j - 1];
+                }
+
+                dp[i][j] = right + down;
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 }
