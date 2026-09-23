57e913aad4986c4fbf3ba08371b4d93d5adc815b
sarthakkotawar2303 <sarthakkotawar2303@gmail.com>
2026-09-23 12:26:39 +0530

    Time: 0 ms (100%), Space: 43.5 MB (64.07%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,6 +1,37 @@
-//RECURSIVE APPRAOCH --> TIME LIMIT EXCEEDS
+// //RECURSIVE APPRAOCH --> TIME LIMIT EXCEEDS
+
+// // class Solution {
+
+// //     public int paths(int n, int m, int[][] obstacleGrid) {
+
+// //         if (m < 0 || n < 0)
+// //             return 0;
+
+// //         if (obstacleGrid[n][m] == 1)
+// //             return 0;
+
+// //         if (m == 0 && n == 0)
+// //             return 1;
+
+// //         int down = paths(n - 1, m, obstacleGrid);
+// //         int right = paths(n, m - 1, obstacleGrid);
+
+// //         return down + right;
+// //     }
+
+// //     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+
+// //         int n = obstacleGrid.length;
+// //         int m = obstacleGrid[0].length;
+// //         if (obstacleGrid[0][0] == 1 || obstacleGrid[n - 1][m - 1] == 1)
+// //             return 0;
+// //         return paths(n - 1, m - 1, obstacleGrid);
+// //     }
+// // }
 
 // class Solution {
+
+//     int[][] dp;
 
 //     public int paths(int n, int m, int[][] obstacleGrid) {
 
@@ -13,10 +44,12 @@
 //         if (m == 0 && n == 0)
 //             return 1;
 
+//         if(dp[n][m]!=-1) return dp[n][m];
+
 //         int down = paths(n - 1, m, obstacleGrid);
 //         int right = paths(n, m - 1, obstacleGrid);
 
-//         return down + right;
+//         return dp[n][m]=down + right;
 //     }
 
 //     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
@@ -25,6 +58,13 @@
 //         int m = obstacleGrid[0].length;
 //         if (obstacleGrid[0][0] == 1 || obstacleGrid[n - 1][m - 1] == 1)
 //             return 0;
+
+//         dp=new int[n][m];
+
+//         for(int[] arr:dp){
+//              Arrays.fill(arr,-1);
+//         }
+
 //         return paths(n - 1, m - 1, obstacleGrid);
 //     }
 // }
@@ -33,24 +73,25 @@ class Solution {
 
     int[][] dp;
 
-    public int paths(int n, int m, int[][] obstacleGrid) {
+    // public int paths(int n, int m, int[][] obstacleGrid) {
 
-        if (m < 0 || n < 0)
-            return 0;
+    //     if (m < 0 || n < 0)
+    //         return 0;
 
-        if (obstacleGrid[n][m] == 1)
-            return 0;
+    //     if (obstacleGrid[n][m] == 1)
+    //         return 0;
 
-        if (m == 0 && n == 0)
-            return 1;
-        
-        if(dp[n][m]!=-1) return dp[n][m];
+    //     if (m == 0 && n == 0)
+    //         return 1;
 
-        int down = paths(n - 1, m, obstacleGrid);
-        int right = paths(n, m - 1, obstacleGrid);
+    //     if (dp[n][m] != -1)
+    //         return dp[n][m];
 
-        return dp[n][m]=down + right;
-    }
+    //     int down = paths(n - 1, m, obstacleGrid);
+    //     int right = paths(n, m - 1, obstacleGrid);
+
+    //     return dp[n][m] = down + right;
+    // }
 
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
 
@@ -58,13 +99,33 @@ class Solution {
         int m = obstacleGrid[0].length;
         if (obstacleGrid[0][0] == 1 || obstacleGrid[n - 1][m - 1] == 1)
             return 0;
-       
-        dp=new int[n][m];
 
-        for(int[] arr:dp){
-             Arrays.fill(arr,-1);
+        dp = new int[n][m];
+        dp[0][0] = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 && j == 0)
+                    continue;
+
+                if (obstacleGrid[i][j] == 1)
+                    continue;
+
+                int right = 0;
+                int down = 0;
+
+                if (i > 0) {
+                    down = dp[i - 1][j];
+                }
+
+                if (j > 0) {
+                    right = dp[i][j - 1];
+                }
+
+                dp[i][j] = right + down;
+            }
         }
-        
-        return paths(n - 1, m - 1, obstacleGrid);
+
+        return dp[n - 1][m - 1];
     }
 }
